23c45da7f209af3855c719073b6263f50a2e2861
@johanleonardois <johanleonardois@ufps.edu.co>
2022-04-05 20:47:21 -0500

    Created slider carousel for the projects section

--- JS/scripts.js
+++ JS/scripts.js
@@ -24,3 +24,36 @@ btnShow.addEventListener('click', () => {
 btnHide.addEventListener('click', () => {
     closeMenu();
 });
+
+//Carousel
+
+let sliderIndex = 0;
+
+const showSlide = (index) => {
+    const slides = document.querySelectorAll('.slide');
+    let dots = document.querySelectorAll('.dot-nav');
+
+    if (index > slides.length - 1) {
+        sliderIndex = 0;
+    }
+    if (index < 0) {
+        sliderIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].classList.remove('active-dot');
+    }
+
+    slides[sliderIndex].style.display = "block";
+    dots[sliderIndex].classList.add('active-dot');
+}
+
+document.querySelectorAll(".dot-nav").forEach((dot) => {
+    dot.addEventListener('click', (e) => {
+        let dots = Array.prototype.slice.call(document.querySelectorAll('.dot-nav')),
+            index = dots.indexOf(e.target);
+        showSlide(sliderIndex = index);
+    });
+});
+
+showSlide(sliderIndex);
